16f7bd8e0b6e52fec556427bc3a5eaa9995ec0dc
kei-g <km.8k6ce+github@gmail.com>
2018-10-05 17:06:05 +0900

    :loud_sound: Set sound type

--- src/main/java/com/snowstep115/ssutils/block/GroundBlockCompressed.java
+++ src/main/java/com/snowstep115/ssutils/block/GroundBlockCompressed.java
@@ -2,9 +2,11 @@ package com.snowstep115.ssutils.block;
 
 import com.snowstep115.ssutils.SnowStepUtils;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
 
 public class GroundBlockCompressed extends BlockCompressed {
     public GroundBlockCompressed() {
         super(Material.GROUND, 0.5f);
+        setSoundType(SoundType.GROUND);
     }
 }
--- src/main/java/com/snowstep115/ssutils/block/RockBlockCompressed.java
+++ src/main/java/com/snowstep115/ssutils/block/RockBlockCompressed.java
@@ -2,9 +2,11 @@ package com.snowstep115.ssutils.block;
 
 import com.snowstep115.ssutils.SnowStepUtils;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
 
 public class RockBlockCompressed extends BlockCompressed {
     public RockBlockCompressed() {
         super(Material.ROCK, 1.5f);
+        setSoundType(SoundType.STONE);
     }
 }
