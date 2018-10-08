b379889018b6b4e6be40d116dc574fdef2869031
kei-g <km.8k6ce+github@gmail.com>
2018-10-08 19:00:03 +0900

    :children_crossing: 石で囲ってからチャンク破壊

--- src/main/java/com/snowstep115/ssutils/item/ChunkDestroyer.java
+++ src/main/java/com/snowstep115/ssutils/item/ChunkDestroyer.java
@@ -1,11 +1,9 @@
 package com.snowstep115.ssutils.item;
 
-import java.util.LinkedList;
-
 import com.snowstep115.ssutils.SnowStepUtils;
-
+import java.util.LinkedList;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockAir;
+import net.minecraft.block.BlockStone;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.entity.Entity;
@@ -17,6 +15,7 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -36,18 +35,63 @@ public class ChunkDestroyer extends ItemBase {
 
     private class Instance implements IInstance {
         private final IFinalizer finalizer;
+        private final EntityPlayer player;
         private int sx, sy, sz, y, z;
 
-        public Instance(int sx, int sy, int sz, IFinalizer finalizer) {
+        public Instance(EntityPlayer player, IFinalizer finalizer) {
             this.finalizer = finalizer;
-            this.sx = ((sx + 15) & ~15) - 16;
-            this.sy = sy + 1;
-            this.sz = ((sz + 15) & ~15) - 16;
-            y = 0;
-            z = 0;
+            this.player = player;
+            this.sx = (((int) player.posX + 15) & ~15) - 16;
+            this.sy = (int) player.posY + 1;
+            this.sz = (((int) player.posZ + 15) & ~15) - 16;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        private void replaceIfLavaOrWater(World world, int x, int y, int z, IBlockState stone) {
+            BlockPos pos = new BlockPos(x, y, z);
+            IBlockState state = world.getBlockState(pos);
+            if (state == null) {
+                player.sendStatusMessage(new TextComponentString(String.format("%s => unable to getBlockState", pos)), false);
+                return;
+            }
+            Block block = state.getBlock();
+            if (block == null) {
+                player.sendStatusMessage(new TextComponentString(String.format("%s => unable to getBlock", pos)), false);
+                return;
+            }
+            ResourceLocation res = block.getRegistryName();
+            if (res == null) {
+                player.sendStatusMessage(new TextComponentString(String.format("%s => unable to getRegistryName", pos)), false);
+                return;
+            }
+            String name = res.getResourcePath();
+            if (name == null) {
+                player.sendStatusMessage(new TextComponentString(String.format("%s => %s, unable to getResourcePath", pos, res)), false);
+                return;
+            }
+            if (name.equals("flowing_lava") || name.equals("flowing_water") || name.equals("lava")
+                    || name.equals("water")) {
+                world.setBlockState(pos, stone);
+            }
+        }
+
+        private void guard(World world) {
+            IBlockState stone = Block.getBlockFromName("minecraft:stone").getDefaultState();
+            for (int y = 0; y < sy; y++) {
+                for (int i = 0; i < 16; i++) {
+                    replaceIfLavaOrWater(world, sx + i, sy - y, sz - 1, stone);
+                    replaceIfLavaOrWater(world, sx + i, sy - y, sz + 16, stone);
+                    replaceIfLavaOrWater(world, sx - 1, sy - y, sz + i, stone);
+                    replaceIfLavaOrWater(world, sx + 16, sy - y, sz + i, stone);
+                }
+            }
         }
 
         public void run(World world) {
+            if (y == 0 && z == 0) {
+                guard(world);
+            }
             for (int x = 0; x < 16; x++) {
                 BlockPos pos = new BlockPos(sx + x, sy - y, sz + z);
                 IBlockState state = world.getBlockState(pos);
@@ -86,7 +130,7 @@ public class ChunkDestroyer extends ItemBase {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
         synchronized (instances) {
-            instances.add(new Instance((int) playerIn.posX, (int) playerIn.posY, (int) playerIn.posZ, new IFinalizer() {
+            instances.add(new Instance(playerIn, new IFinalizer() {
                 public void run(IInstance instance) {
                     synchronized (instances) {
                         instances.remove(instance);
