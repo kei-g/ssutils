c29fc7679a470e3e8b9a143d94dabee7b4c09868
kei-g <km.8k6ce+github@gmail.com>
2018-10-08 15:41:51 +0900

    :children_crossing: ブロックのNBTを表示する

--- src/main/java/com/snowstep115/ssutils/item/Yukiho.java
+++ src/main/java/com/snowstep115/ssutils/item/Yukiho.java
@@ -11,10 +11,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -76,5 +82,23 @@ public class Yukiho extends ItemPickaxe {
             block.dropBlockAsItem(world, pos, st, FORTUNE_LEVEL);
         }
         return result;
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
+            EnumFacing facing, float hitX, float hitY, float hitZ) {
+        TileEntity tile = worldIn.getTileEntity(pos);
+        if (tile != null) {
+            NBTTagCompound tag = tile.serializeNBT();
+            String msg = String.format("[%s] %s", worldIn.isRemote ? "REMOTE" : "LOCAL", tag);
+            player.sendStatusMessage(new TextComponentString(msg), false);
+        }
+        return EnumActionResult.SUCCESS;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        ItemStack item = playerIn.getHeldItem(handIn);
+        return ActionResult.newResult(EnumActionResult.SUCCESS, item);
     }
 }
